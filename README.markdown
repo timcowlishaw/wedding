4487bcdd4fa6b298bc641c2393a859aef8e212f5
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-09-22 19:22:04 +0100

    rails /rspec/cucumber stuff

---
+++ README.markdown
@@ -0,0 +1,6 @@
+Tim and Vicky's Wedding site
+============================
+
+A little rails app that we will use to announce details of our wedding to guests, and collect RSVPs.
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = 'df9695098d09ce649099fe40ded82b1b436114effc3b3850743e302ec9c8b590e522508073c72f746826eaca9dafe32d087b515193d4ca075e10434867008cf7'
